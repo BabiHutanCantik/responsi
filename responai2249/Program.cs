db3672131e47bca42a7721e1ef50300652626c19
mbinggg <akbr.hac17@gmail.com>
2019-05-01 23:33:11 +0700

    Add project files.

---
+++ responai2249/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responai2249
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
